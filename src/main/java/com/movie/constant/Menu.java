b4464c4b7050d2bd147931c200f3579323ab99f5
yunune96 <89775838+yunune96@users.noreply.github.com>
2025-06-30 09:32:30 +0900

    Revert "feat/store"

--- src/main/java/com/movie/constant/Menu.java
+++
@@ -1,5 +0,0 @@
-package com.movie.constant;
-
-public enum Menu {
-    COMBO,POPCORN,DRINK,SNACK
-}
--- src/main/java/com/movie/exception/OutOfStockException.java
+++
@@ -1,7 +0,0 @@
-package com.movie.exception;
-
-public class OutOfStockException extends RuntimeException{
-    public OutOfStockException(String message){
-        super(message);
-    }
-}
