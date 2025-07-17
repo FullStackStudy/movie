5002ef3608138c9b5d31b3c688d8d06a968b7c47
daeun <maryy1016@gmail.com>
2025-07-17 11:18:58 +0900

    모달 수정, openaiservice 수정

--- src/main/java/com/movie/service/chatbot/OpenAiService.java
+++ src/main/java/com/movie/service/chatbot/OpenAiService.java
@@ -29,7 +29,8 @@ public class OpenAiService {
                 너는 MovieFlex 영화관 웹사이트의 친절한 챗봇이야.
                 사용자에게 영화 정보, 시간표, 회원가입, 로그인, 스토어 관련 내용을 도와줘야 해.
                 영화관은 인천, 성남, 노량진, 강남, 부산에 있어.
-                URL 링크는 실제 ResponseType.LINK 타입을 기반에서 제공하니 키워드만 발췌해서 안내만 해줘.
+                URL 링크는 실제 ResponseType.LINK 타입을 기반에서 제공하니 키워드만 발췌해서 
+                꼭 존댓말로 안내만 해줘 안된다는 말 금지.
                 """;
 
         String fullPrompt = systemPrompt + "\n\n" + message;
@@ -79,7 +80,7 @@ public class OpenAiService {
             return ChatbotResponseDto.builder()
                     .response(gptAnswer)
                     .buttonText("회원가입 하러가기")
-                    .buttonUrl("/members/new")
+                    .buttonUrl("/members/auth")
                     .type(ResponseType.LINK)
                     .build();
         }
@@ -89,13 +90,13 @@ public class OpenAiService {
             return ChatbotResponseDto.builder()
                     .response(gptAnswer)
                     .buttonText("로그인 하러가기")
-                    .buttonUrl("/members/login")
+                    .buttonUrl("/members/auth")
                     .type(ResponseType.LINK)
                     .build();
         }
 
         /* 스토어 관련 Question */
-        if (lower.contains("팝콘")) {
+        if (lower.contains("팝콘") || lower.contains("스토어")) {
             return ChatbotResponseDto.builder()
                     .response(gptAnswer)
                     .buttonText("스토어 가기")
@@ -110,6 +111,35 @@ public class OpenAiService {
                     .response(gptAnswer)
                     .buttonText("영화 찾기")
                     .buttonUrl("/movie")
+                    .type(ResponseType.LINK)
+                    .build();
+        }
+
+        /* 마이페이지 관련 Question */
+        if (lower.contains("마이페이지") || lower.contains("회원정보")) {
+            return ChatbotResponseDto.builder()
+                    .response(gptAnswer)
+                    .buttonText("마이페이지")
+                    .buttonUrl("/mypage")
+                    .type(ResponseType.LINK)
+                    .build();
+        }
+
+        /* 예매 정보 관련 마이페이지 반환 */
+        if (lower.contains("예매 내역")) {
+            return ChatbotResponseDto.builder()
+                    .response(gptAnswer)
+                    .buttonText("마이페이지")
+                    .buttonUrl("/mypage")
+                    .type(ResponseType.LINK)
+                    .build();
+        }
+
+        if (lower.contains("추천")) {
+            return ChatbotResponseDto.builder()
+                    .response(gptAnswer)
+                    .buttonText("AI 추천")
+                    .buttonUrl("/recommend")
                     .type(ResponseType.LINK)
                     .build();
         }
@@ -172,4 +202,4 @@ public class OpenAiService {
         }
     }
 
-} 
+}
